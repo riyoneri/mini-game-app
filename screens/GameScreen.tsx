7e5fcbfcf8bc9e0e51d539ff13cb4d434b7e56ba
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-27 19:28:53 +0200

    refactor: create generating random number functionalities

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import Title from "../components/ui/Title";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
 interface GameScreenProperties {
   userNumber: number;
@@ -15,10 +16,43 @@ function generateRandomBetween(min: number, max: number, exclude: number) {
   return randomNumber;
 }
 
+// let minBoundary = 1;
+// let maxBoundary = 100;
+
 export default function GameScreen({ userNumber }: GameScreenProperties) {
-  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const minBoundary = useRef(1);
+  const maxBoundary = useRef(100);
+
+  const initialGuess = generateRandomBetween(
+    minBoundary.current,
+    maxBoundary.current,
+    userNumber
+  );
 
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction: "lower" | "greater") {
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "greater" && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { style: "cancel", text: "Cancel" },
+      ]);
+      return;
+    }
+
+    if (direction === "lower") maxBoundary.current = currentGuess;
+    else minBoundary.current = currentGuess + 1;
+
+    const nextGuess = generateRandomBetween(
+      minBoundary.current,
+      maxBoundary.current,
+      currentGuess
+    );
+
+    setCurrentGuess(nextGuess);
+  }
 
   return (
     <View style={styles.screen}>
@@ -26,7 +60,12 @@ export default function GameScreen({ userNumber }: GameScreenProperties) {
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
-        {/* + - */}
+        <PrimaryButton onPress={() => nextGuessHandler("lower")}>
+          -
+        </PrimaryButton>
+        <PrimaryButton onPress={() => nextGuessHandler("greater")}>
+          +
+        </PrimaryButton>
       </View>
       {/* <View>LOG ROUNDS</View> */}
     </View>
