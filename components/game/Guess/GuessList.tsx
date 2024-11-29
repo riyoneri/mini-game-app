bf2ce9b8b23882a2d085210ca281fb72fedf2f7f
Lionel Kaneza <lionkaneza@gmail.com>
2024-11-29 08:22:11 +0200

    feat: create GuessList component

---
+++ components/game/Guess/GuessList.tsx
@@ -0,0 +1,29 @@
+import { FlatList, StyleSheet, View } from "react-native";
+import { Guess } from "../../../App";
+import GuessListItem from "./GuessListItem";
+
+interface GuessListProperties {
+  allGuess: Guess[];
+}
+
+export default function GuessList({ allGuess }: GuessListProperties) {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        contentContainerStyle={styles.listContent}
+        data={allGuess}
+        renderItem={(guess) => <GuessListItem {...guess.item} />}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 16,
+    gap: 30,
+  },
+  listContent: { gap: 15 },
+});
