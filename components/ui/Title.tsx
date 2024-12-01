5eb12e87debe4080d8273849a16aa860493906ae
Lionel Kaneza <lionkaneza@gmail.com>
2024-12-01 13:03:23 +0200

    style: make GameOverScreen responsive

--- components/ui/Title.tsx
+++ components/ui/Title.tsx
@@ -1,6 +1,5 @@
 import { PropsWithChildren } from "react";
 import { StyleSheet, Text } from "react-native";
-import Colors from "../../constants/colors";
 
 export default function Title({ children }: PropsWithChildren) {
   return <Text style={styles.title}>{children}</Text>;
